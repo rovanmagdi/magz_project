216df836f9bcf442b969615e513d56eef2eb5965
Omniah Mahmoud <omniamahmoud410@gmail.com>
2022-06-11 19:19:43 +0200

    test

--- src/styled/styledPaper.tsx
+++ src/styled/styledPaper.tsx
@@ -1,16 +1,12 @@
-import { styled } from "@mui/system";
-import { Paper } from "@mui/material";
+import { styled } from "@mui/material";
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 
-export const ProfileDataItem = styled(Paper)(() => ({
-    // padding:'10px',
-    paddingLeft:'20px',
-    minHeight:'45px',
-    lineHeight:'45px'
-   
-   }));
-   
-   export const WeatherPaper = styled(Paper)(()=>({
 
-    width:"270px" , padding:"25px", height:"200px"
-    
-    }))
+
+
+export const WeatherPaper = styled(Paper)(()=>({
+
+width:"270px" , padding:"25px", height:"200px"
+
+}))
--- src/styled/styledTypography.tsx
+++ src/styled/styledTypography.tsx
@@ -1,28 +1,11 @@
-import { styled } from "@mui/system";
-import Typography from "@mui/material/Typography";
-
-export const PostTitle = styled(Typography)(({ theme }) => ({
-  fontWeight: "bold",
-  fontSize: ".9em",
-  // width:'250px',
-}));
-
-export const PostDate = styled(Typography)(({ theme }) => ({
-  fontSize: ".8em",
-  color: "gray",
-}));
-
-export const AutherName = styled(Typography)(({ theme }) => ({
-  fontSize: "1em",
-  color: "black",
-}));
-
+import { Typography } from "@mui/material";
+import { styled } from "@mui/material";
 
 
 export const TypographyCardTitle= styled(Typography)(()=>({
 
 
-  fontSize:"1.1em"  ,
+    fontSize:"1.1em"  ,
 fontWeight:"700", overflow:"hidden", height:"45px"
 
 
