ecc730149113e3518935ffe7a2386d85b87202f4
Omniah Mahmoud <omniamahmoud410@gmail.com>
2022-06-11 19:50:50 +0200

    save

--- src/styled/styledTypography.tsx
+++ src/styled/styledTypography.tsx
@@ -11,18 +11,3 @@ fontWeight:"700", overflow:"hidden", height:"45px"
 
 
 }))
-export const PostTitle = styled(Typography)(({ theme }) => ({
-  fontWeight: "bold",
-  fontSize: ".9em",
-  // width:'250px',
-}));
-
-export const PostDate = styled(Typography)(({ theme }) => ({
-  fontSize: ".8em",
-  color: "gray",
-}));
-
-export const AutherName = styled(Typography)(({ theme }) => ({
-  fontSize: "1em",
-  color: "black",
-}));
