7ba94c4b9f787154b4af23c61f69a838f41d0a1e
Rovan Magdy <magdirovan@gmail.com>
2022-06-14 22:00:45 +0200

    responsive_login

--- src/pages/Sign/Login.tsx
+++ src/pages/Sign/Login.tsx
@@ -6,24 +6,37 @@ import { Home, Info,Content } from "../../styled/HomePage";
 
 import MniNavbar from "../../components/mniNabarSign/mniNavbar";
 import { StyledLoginContainer } from "../../styled/Login";
-import {Box} from"@mui/material"
+import {Box, useMediaQuery} from"@mui/material"
+import { theme } from "../../theme/palette";
+import LoginResponsive from "../../components/loginResponsive/LoginResponsive";
 
 export default function Login() {
+  const isMatch = useMediaQuery(theme.breakpoints.down(1117));
+
   return (
     <>
       <Home />
       <Content>
-       
+
+       {!isMatch?(
+        
         <StyledLoginContainer>
           <NavbarSign />
           <Box sx={{ display: "flex" }}>
             <ImageSign />
             <MniNavbar />
           </Box>
-
           <FooterSign />
-        </StyledLoginContainer>
+        </StyledLoginContainer>):(
+        <StyledLoginContainer>
+          <NavbarSign />
+          <Box >
+           <LoginResponsive/>
+          </Box>
+          <FooterSign />
+        </StyledLoginContainer>)}
       </Content>
+
     </>
   );
 }
