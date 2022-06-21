2e8dbb0a8a81a6d59161b1ccf063370b2d61c718
Omniah Mahmoud <omniamahmoud410@gmail.com>
2022-06-21 14:32:29 +0200

    testdata

--- src/styled/styledTypography.tsx
+++ src/styled/styledTypography.tsx
@@ -1,14 +1,12 @@
 import { Typography } from "@mui/material";
 import { styled } from "@mui/material";
 
-
 export const PostTitle = styled(Typography)(({ theme }) => ({
   fontWeight: "bold",
   fontSize: ".9em",
   // width:'250px',
-  // ahmed
+  //ahmed
 }));
-
 
 export const PostDate = styled(Typography)(({ theme }) => ({
   fontSize: ".8em",
@@ -22,21 +20,20 @@ export const AutherName = styled(Typography)(({ theme }) => ({
   //ahmed
 }));
 
-export const TypographyCardTitle= styled(Typography)(()=>({
+export const TypographyCardTitle = styled(Typography)(() => ({
+  fontSize: "1.1em",
+  fontWeight: "700",
+  overflow: "hidden",
+  height: "45px",
 
+  //ahmed
+}));
 
-    fontSize:"1.1em"  ,
-fontWeight:"700", overflow:"hidden", height:"45px"
+export const DateTypography = styled(Typography)(({ theme }) => ({
+  fontWeight: "600",
+  fontSize: "1em",
+  color: `${theme.palette.secondary.dark}`,
+  marginBottom: "3px",
 
-
-
-}))
-
-
-export const DateTypography= styled(Typography)(({theme})=>({
-
-  fontWeight:"600", fontSize:"1em", color:`${theme.palette.secondary.dark}` ,marginBottom:"3px"
-
-
-
-}))
+  //ahmed
+}));
