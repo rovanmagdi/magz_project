c48e35f8858c5348a10059bce84e93b3fc7b2a46
Omniah Mahmoud <omniamahmoud410@gmail.com>
2022-06-21 14:56:50 +0200

    addFiles

---
+++ src/styled/styledTypography.tsx
@@ -0,0 +1,32 @@
+import { Typography } from "@mui/material";
+import { styled } from "@mui/material";
+
+export const PostTitle = styled(Typography)(({ theme }) => ({
+  fontWeight: "bold",
+  fontSize: ".9em",
+  // width:'250px',
+  // ahmed
+  // ahmed
+}));
+
+export const PostDate = styled(Typography)(({ theme }) => ({
+  fontSize: ".8em",
+  color: "gray",
+  // ahmed
+  //ahmed
+}));
+
+export const AutherName = styled(Typography)(({ theme }) => ({
+  fontSize: "1em",
+  color: "black",
+  // ahmed
+  //ahmed
+}));
+
+export const TypographyCardTitle = styled(Typography)(() => ({
+  fontSize: "1.1em",
+  fontWeight: "700",
+  overflow: "hidden",
+  height: "45px",
+  // ahmed
+}));
